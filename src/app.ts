6d83ff03551c55d272d11dec2441292d07be99ba
twahanur <thohanur143@gmail.com>
2024-03-14 12:40:17 +0600

    update origin

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,12 @@ const app: Application = express();
 
 //server site
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(
+  cors({
+    origin: 'https://vercel.com/twahanurs-projects/look-at-me-client',
+    credentials: true,
+  }),
+);
 
 // application routes
 app.use('/api', router);
