b718553bd304cfe87e6f8cdd0919581c71907e31
twahanur <thohanur143@gmail.com>
2024-03-14 13:14:59 +0600

    origin change

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: 'https://look-at-me-client.vercel.app',
+    origin: '*',
     credentials: true,
   }),
 );
